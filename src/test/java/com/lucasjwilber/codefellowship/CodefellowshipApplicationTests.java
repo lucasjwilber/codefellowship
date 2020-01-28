d7ac4913e674ac462943c4a4126f81a1c3cf80e9
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-28 10:43:36 -0800

    initial commit

---
+++ src/test/java/com/lucasjwilber/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasjwilber.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
